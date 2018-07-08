8c415c23bae4e2e07c53075ad1601c5488d81a85
nilesflow <nilesflow@gmail.com>
2018-07-09 03:41:32 +0900

    for pip

--- setup.py
+++ setup.py
@@ -1,2 +1,15 @@
-from setuptools import setup
-setup()
+#!usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name = "pyfw",
+    version = "0.0.1",
+    description = "nilesflow: Personal Python Framework",
+    url = "https://github.com/nilesflow/pyfw",
+    author = "nilesflow",
+    author_email = "nilesflow@gmail.com",
+    license = "MIT",
+    long_description = open('README.md').read(),
+    long_description_content_type="text/markdown",
+    packages = find_packages(),
+)
